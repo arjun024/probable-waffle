3415e0ccdcc3dd4f8a64e8043816f4c0a037576a
Benjamin Cheung <bcheung@umass.edu>
2018-04-27 21:24:36 -0400

    Removed old stuff, not needed. Added framework of the base script

---
+++ SeeDB.py
@@ -0,0 +1,22 @@
+
+#Input the name of the full dataset
+#And a psycopg2 connection conn
+#Will take the whole table and partition the data into n subsets by creating n views
+#Output an array of view names
+def partitioner(fullTable, conn, n):
+	return 1
+
+#Performs the n phase transitions. 
+#Using C.I., some aggregrate views will be discarded
+#The KL score is calculated for the subset on each query pair
+#The ones with "low(?)" utility will be removed from later iterations
+#output: Top k query, at the end of the n phases. 
+def phaser(fullTable, conn, n, k):
+
+
+
+
+
+def main():
+
+
--- g1.sql
+++
@@ -1,2 +0,0 @@
-create view adultg as 
-select *, case when marital_status like '%Married%' then 0 else 1 end as g1 from adult;
