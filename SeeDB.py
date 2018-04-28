85f178311dbc3e303eebb047b02f9507a2991a3d
Ben <b.cheung2@gmail.com>
2018-04-28 14:38:16 -0400

    Finished the partitioning portion. Changed readme, altered table to have row numbers as well. Note, uncomment the area that actualt writes to the database if you want the real effect. otherwise, it's commented out to not do that in testing.

--- SeeDB.py
+++ SeeDB.py
@@ -1,22 +1,68 @@
+import sys
+import pdb
+import psycopg2
+import math
+import pandas.io.sql as sqlio
 
 #Input the name of the full dataset
 #And a psycopg2 connection conn
 #Will take the whole table and partition the data into n subsets by creating n views
 #Output an array of view names
 def partitioner(fullTable, conn, n):
-	return 1
+	sql = "select count(*) from adult;"
+	df = sqlio.read_sql_query(sql, conn)
+	totalRows = df["count"].values[0]
+	size = math.floor(totalRows/n)
+	leftOver = totalRows%n
+	index = []
+	print("size", size)
+	for i in range(n):
+		print(leftOver, i)
+		if leftOver > i:
+			if i != 0:
+				end = index[i-1][1]
+				index.append((1+end, 1 + end+size))
+			else: 
+				index.append((1, size+1))
+		else: 
+			if i != 0:
+				end = index[i-1][1]
+				index.append((1+end, end+size))
+			else:
+				index.append((1, size))
+
+	allViews = [] 
+	for i in range(n):
+		sql = "create view part_" + str(i) + " as select * from adult where id between " + str(index[i][0]) + " and " + str(index[i][1]) + ";" 
+		# print(sql)
+		# sqlio.read_sql_query(sql, conn)
+		allViews.append("part_"+str(i))
+
+	return allViews
+
+
+
 
 #Performs the n phase transitions. 
 #Using C.I., some aggregrate views will be discarded
 #The KL score is calculated for the subset on each query pair
 #The ones with "low(?)" utility will be removed from later iterations
 #output: Top k query, at the end of the n phases. 
-def phaser(fullTable, conn, n, k):
-
+def phaser(fullTable, conn, k, allViews):
+	
 
 
 
 
 def main():
+	conn = psycopg2.connect("dbname=adult user=ben password=postgres")
+	k = int(sys.argv[1])
+	n = int(sys.argv[2])
+	allViews = partitioner("adult", conn, n)
+	# print(allViews)
+	tok_k = phaser("adult", conn, k, allViews)
 
+
+if __name__ == '__main__':
+	main()
 
